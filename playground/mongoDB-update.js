2ab3b0dbd35a6fd71b3224b3b932b0a48ac568b0
pranav madhani <you@example.com>
2019-07-18 10:15:22 +0530

    with update operation

---
+++ playground/mongoDB-update.js
@@ -0,0 +1,45 @@
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (error, client) => {
+    if (error) {
+        return console.log("unable to connect to mongoDB server..", error)
+    }
+    console.log("connected to server")
+    const db = client.db('TodoApp')
+
+
+//    db.collection('Todos').find({
+//        _id:new ObjectID('5d2eaa38682cde16b8189cbe')}
+//        ).toArray().then((docs)=>
+//    {
+   
+//     console.log(JSON.stringify(docs,undefined,2))
+
+//    },(err)=>{
+//        console.log('unable to fetch',err)
+//    });
+
+
+
+
+db.collection('Todos').findOneAndReplace({text: "something to insert"},
+{
+   
+        text: "Hit gym at 6"
+
+   
+},false
+).then((data)=>{
+
+
+    console.log("update",data) // check
+    
+
+}).catch(function(err) {
+    console.log(err.message); // some coding error in handling happened
+  })
+   
+
+client.close();
+})
