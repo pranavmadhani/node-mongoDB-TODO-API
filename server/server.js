eb61b1ddb7a0fc5823f57641bcfa0c7bdbbf027c
pranav madhani <you@example.com>
2019-07-19 12:46:14 +0530

    delete api added

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var{User}  = require('./models/users')
 var express = require('express')
 var bodyParser = require('body-parser')
 var obID= require('mongodb').ObjectID;
-
+const port = process.env.PORT||3000;
 var app=express();
 app.use(bodyParser.json());
 
@@ -30,6 +30,7 @@ app.get('/api/todos',(req,res)=>{
   Todo.find().then((todos)=>
   {
       res.send({todos})
+      console.log(todos.length)
   },(err)=>{
   
       res.status(400).send(err);
@@ -61,9 +62,44 @@ app.get('/api/todos/:id',(req,res)=>{
 })
 
 
-app.listen(3000,()=>
+app.delete('/api/todos/:id',(req,res)=>
 {
-    console.log("connection established at 3000")
+  var id=req.params.id;
+ 
+  if(!obID.isValid(id))
+  return res.status(404).send('invalid ID')
+
+  
+
+  Todo.findByIdAndDelete(id).then((doc)=>{
+
+    if(!doc)return res.status(404).send({
+
+      message:'error: the node is either already deleted or not present'
+
+    }); 
+
+    console.log('success....')
+    console.log(doc)
+    res.send({doc})
+
+
+  }).catch((err)=>{
+    
+
+    res.status(400).send({
+      
+    });
+   });
+})
+
+
+
+
+
+app.listen(port,()=>
+{
+    console.log(`connection established at ${port}` )
 })
  
 
